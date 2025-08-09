9121b99e4076ceef62cbc3aa70d2ddce5f17eb59
ch1nya <nasferatum382@gmail.com>
2025-08-09 15:53:40 +0300

    Clean up

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/authFormExample/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  server: {
+    port: 3000
+  }
 })
